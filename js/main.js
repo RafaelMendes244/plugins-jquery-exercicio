23d515d32fd81659661a558675824363e94ecd26
Rafael <gabriel.mito07@gmail.com>
2025-06-14 05:10:31 -0300

    Correção: Corrigindo ERROS do Modulo 10 da EBAC

--- js/main.js
+++ js/main.js
@@ -1,42 +1,73 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
+$(document).ready(function () {
+            $('#carousel-imagens').slick({
+                autoplay: true,
+                arrows: false
+            })
+        })
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
+        $('#telefone').mask('(00) 00000-0000', {
+            placeholder: '(00) 00000-0000'
+        });
+        $('#cep').mask('00000-000', {
+            placeholder: '00000-000'
+        });
+        $('#cpf').mask('000.000.000-00', {
+            placeholder: '000.000.000-00'
+        });
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
-
-$('form').validate({
-    rules: {
-        nome: {
-            requerido: true
-        },
-        email: {
-            requerido: true,
-            email: true
-        },
-        telefone: {
-            requerido: true
-        },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
-    },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
-    },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
-})
+        $('form').validate({
+            rules: {
+                nome: {
+                    required: true,
+                    minlength: 3
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                telefone: {
+                    required: true,
+                    minlength: 14
+                },
+                cep: {
+                    required: true,
+                    minlength: 9
+                },
+                cpf: {
+                    required: true,
+                    minlength: 14
+                }
+            },
+            messages: {
+                nome: {
+                    required: 'Por favor, informe seu nome',
+                    minlength: 'Seu nome deve ter pelo menos 3 caracteres'
+                },
+                email: {
+                    required: 'Por favor, informe seu e-mail',
+                    email: 'Por favor, informe um e-mail válido'
+                },
+                telefone: {
+                    required: 'Por favor, informe seu telefone',
+                    minlength: 'Telefone inválido'
+                },
+                cep: {
+                    required: 'Por favor, informe seu CEP',
+                    minlength: 'CEP inválido'
+                },
+                cpf: {
+                    required: 'Por favor, informe seu CPF',
+                    minlength: 'CPF inválido'
+                }
+            },
+            submitHandler: function (form) {
+                alert('Formulário enviado com sucesso!');
+                form.submit();
+            },
+            invalidHandler: function (event, validator) {
+                let errors = validator.numberOfInvalids();
+                if (errors) {
+                    alert('Faltou Preencher ' + errors + ' campo(s) obrigatório(s).');
+                }
+            }
+        });
